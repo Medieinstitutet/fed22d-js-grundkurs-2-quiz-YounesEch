1db80b64752e0a317bc4d8217d73c843a913574a
YounesEch <114940175+YounesEch@users.noreply.github.com>
2022-12-16 20:42:03 +0100

    fixat så att frågorna kommer i en slumpmässig ordning

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import './style/style.scss';
+import { questions } from './array';
+import { shuffle } from './utils';
 
 const darkMode = document.querySelector('#darkMode');
 const lightMode = document.querySelector('#lightMode');
@@ -34,109 +36,6 @@ next?.addEventListener('click', nextQuestion);
 
 let currentQuestion = 0; 
 let points = 0;
-
-const questions = [
-  {
-    questionText: 'What is the capital of Sweden?', 
-    answerOptions: [
-      'Malmö',
-      'Västerås',
-      'Stockholm',
-      'Göteborg',
-    ], 
-    correctAnswer: 'Stockholm',
-  }, 
-  {
-    questionText: 'What is the capital of Morocco?', 
-    answerOptions: [
-      'Casablanca',
-      'Rabat',
-      'Marrakech',
-      'Tangier',
-    ], 
-    correctAnswer: 'Rabat',
-  },
-  {
-    questionText: 'What is the capital of France?', 
-    answerOptions: [
-      'Paris',
-      'Marseille',
-      'Strasbourg',
-      'Lyon',
-    ],
-    correctAnswer: 'Paris',
-  },
-  {
-    questionText: 'Where is Spain?', 
-    answerOptions: [
-      'Africa',
-      'Asia',
-      'America',
-      'Europa',
-    ],
-    correctAnswer: 'Europa',
-  },
-  {
-    questionText: 'Where is Tunisia?', 
-    answerOptions: [
-      'Africa',
-      'Asia',
-      'Europa',
-      'America',
-    ],
-    correctAnswer: 'Africa',
-  },
-  {
-    questionText: 'What is the biggest town in Morocco?', 
-    answerOptions: [
-      'Rabat',
-      'Casablanca',
-      'Agadir',
-      'Tangier',
-    ],
-    correctAnswer: 'Casablanca',
-  },
-  {
-    questionText: 'Which countries border to Turkey?', 
-    answerOptions: [
-      'Irak',
-      'Italy',
-      'Bulgaria',
-      'Serbia',
-    ],
-    correctAnswer: 'Irak, Bulgaria',
-  },
-  {
-    questionText: 'Which country border to Portugal?',
-    answerOptions: [
-      'Spain',
-      'Morocco',
-      'France',
-      'United kingdom',
-    ],
-    correctAnswer: 'Spain',
-  },
-  {
-    questionText: 'What is the river in France called?', 
-    answerOptions: [
-      'Nilen',
-      'Loire',
-      'Elbe',
-      'Main',
-    ], 
-    correctAnswer: 'Loire',
-  },
-  {
-    questionText: 'What is the biggest town in the world?', 
-    answerOptions: [
-      'Tokyo',
-      'New Dehli',
-      'Jakarta',
-      'Mumbai',
-    ], 
-    correctAnswer: 'tokyo ',
-  }
-];
 
 document.querySelector ('#restartGameBtn').addEventListener('click', restartGame);
 
@@ -183,7 +82,7 @@ console.log(points);
 }
 
 function nextQuestion() {
-  if (currentQuestion >= questions.length) { 
+  if (currentQuestion >= shuffle(questions).length) { 
     gameOver();
     return;
   }
