051c445cea5280ffef57605b155425dfde75ac7d
YounesEch <114940175+YounesEch@users.noreply.github.com>
2022-12-30 21:19:12 +0100

    ändrat i readMe filen

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-/**********************************************************Import***************************************************************************************/
+/**********************************************Import******************************************************************/
 import './style/style.scss';
 import { questions } from './array';
 import { shuffle } from './utils';
-/**********************************************************Variabler***************************************************************************************/
+/*******************************************Variabler******************************************************************/
 const darkMode = document.querySelector('#darkMode');
 const lightMode = document.querySelector('#lightMode');
 const yesBtn = document.querySelector('#acceptedBtn');
@@ -11,63 +11,43 @@ const noBtn = document.querySelector('#deniedBtn');
 const gameDescText = 'Let the Game Begin!';
 const gameDescription = document.querySelector('#gameDescription');
 
-let playerName = '';
-
 const questionTextDiv = document.querySelector('#questionText');
 const answer1Btn = document.querySelector('#answer1Text');
 const answer2Btn = document.querySelector('#answer2Text');
 const answer3Btn = document.querySelector('#answer3Text');
 const answer4Btn = document.querySelector('#answer4Text');
 
-const questionCounter= document.querySelector('#counter');
-const maxQuestions= 10;
+const questionCounter = document.querySelector('#counter');
+const btn = document.querySelector('.answerContainer');
+const maxQuestions = 10;
 
-let currentQuestion = 0; 
+const tickingTime = document.querySelector('.tickingTime');
+const timer = document.getElementById('timer');
+
+let playerName = '';
+let sec = 60;
+let currentQuestion = 0;
 let points = 0;
 
-/**********************************************************Event***************************************************************************************/
-
-darkMode.addEventListener('click', backgroundDark);
-lightMode.addEventListener('click', backgroundLight);
-
-yesBtn.addEventListener('click', goToGame);
-noBtn.addEventListener('click', nextTime);
-
-document.querySelector('#startGameBtn').addEventListener('click', startGame);
-
-answer1Btn.addEventListener('click', checkAnswer);
-answer2Btn.addEventListener('click', checkAnswer);
-answer3Btn.addEventListener('click', checkAnswer); 
-answer4Btn.addEventListener('click', checkAnswer); 
-
-
-answer1Btn.addEventListener('click', nextQuestion);
-answer2Btn.addEventListener('click', nextQuestion);
-answer3Btn.addEventListener('click', nextQuestion);
-answer4Btn.addEventListener('click', nextQuestion);
-
-
-document.querySelector ('#restartGameBtn').addEventListener('click', restartGame);
-
-gameDescription.innerHTML = gameDescText;
-
-/**********************************************************Functions***************************************************************************************/
-function backgroundDark(){
+/***************************************** Functions ******************************************************************/
+function backgroundDark() {
   document.body.classList.toggle('light_theme');
   darkMode.classList.toggle('display');
   lightMode.classList.remove('display');
 }
 
-function backgroundLight(){
+function backgroundLight() {
   document.body.classList.remove('light_theme');
   lightMode.classList.toggle('hide');
   darkMode.classList.remove('hide');
 }
 
-function goToGame(){
+function goToGame() {
   document.querySelector('#pageOne').style.display = 'none';
   document.querySelector('#pageTwo').style.display = 'block';
 }
+
+/**ger oss en aleert box när vi avböjer att starta spelet.* */
 
 function nextTime() {
   alert('Better luck next time, wish you all luck');
@@ -81,28 +61,20 @@ function startGame() {
   document.body.style.backgroundImage ='none';
   tickingTime.classList.remove('hide');
   nextQuestion();
-  timer = setInterval(myTimer,500);
+  timer = setInterval(setTimer, 500);
 }
 
-const tickingTime = document.querySelector('.tickingTime');
-const timer = document.getElementById('timer');
-let sec = 60;
-
-function myTimer() {
-    timer.innerHTML = sec + ' sekunder';
-    sec--;
-    if (sec === 0) {
-        clearInterval(timer);
-        alert('Tyvärr tiden är slut!');
-    }
-}
-
-function stopMyTimer(){
-  clearInterval(myTimer);
+function setTimer() {
+  timer.innerHTML = sec + 'seconds';
+  sec --;
+  if (sec === 0) {
+    clearInterval(timer);
+    alert('The time is up!');
+  }
 }
 
 function nextQuestion() {
-  if (currentQuestion >= shuffle(questions).length) { 
+    if (currentQuestion >= shuffle(questions).length) { 
     gameOver();
     return;
   }
@@ -113,11 +85,8 @@ function nextQuestion() {
   answer4Btn.innerHTML = questions[currentQuestion].answerOptions[3];
 
   currentQuestion ++;
-  questionCounter.innerHTML= `${currentQuestion}/${maxQuestions}`;
+  questionCounter.innerHTML = `${currentQuestion}/${maxQuestions}`;
   }
-
-  const btn = document.querySelector('.answerContainer');
-  
 
 function checkAnswer(e){
   const userAnswer = e.currentTarget.innerHTML;
@@ -153,5 +122,30 @@ function gameOver() {
   document.querySelector('#pointsContainer').innerHTML = `Grattis ${playerName} du fick ${points} poäng!!`;
   document.querySelector('#restartGameBtn').style.display = 'block';
   document.querySelector('#next').style.display = 'none';
-
 }
+
+/*********************************************** Event ****************************************************************/
+
+darkMode.addEventListener('click', backgroundDark);
+lightMode.addEventListener('click', backgroundLight);
+
+yesBtn.addEventListener('click', goToGame);
+noBtn.addEventListener('click', nextTime);
+
+document.querySelector('#startGameBtn').addEventListener('click', startGame);
+
+answer1Btn.addEventListener('click', checkAnswer);
+answer2Btn.addEventListener('click', checkAnswer);
+answer3Btn.addEventListener('click', checkAnswer); 
+answer4Btn.addEventListener('click', checkAnswer); 
+
+
+answer1Btn.addEventListener('click', nextQuestion);
+answer2Btn.addEventListener('click', nextQuestion);
+answer3Btn.addEventListener('click', nextQuestion);
+answer4Btn.addEventListener('click', nextQuestion);
+
+
+document.querySelector ('#restartGameBtn').addEventListener('click', restartGame);
+
+gameDescription.innerHTML = gameDescText;
